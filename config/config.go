cec3d7792ca34bd1b8b95b4d01ec29791a12c25c
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2021-12-03 21:02:56 +0500

    config directory added with mongo configuration

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	Environment string
+	LogLevel    string
+	HttpPort    string
+
+	MongoHost     string
+	MongoPort     int
+	MongoUser     string
+	MongoPassword string
+	MongoDatabase string
+}
+
+func Load() Config {
+
+	cfg := Config{}
+
+	cfg.Environment = cast.ToString(getOrReturnDefault("ENVIRONMENT", "develop"))
+
+	cfg.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
+
+	cfg.HttpPort = cast.ToString(getOrReturnDefault("HTTP_PORT", ":8000"))
+
+	cfg.MongoHost = cast.ToString(getOrReturnDefault("MONGO_HOST", "localhost"))
+	cfg.MongoPort = cast.ToInt(getOrReturnDefault("MONGO_PORT", 27017))
+	cfg.MongoUser = cast.ToString(getOrReturnDefault("MONGO_USER", "espace"))
+	cfg.MongoPassword = cast.ToString(getOrReturnDefault("MONGO_PASSWORD", "mongodb"))
+	cfg.MongoDatabase = cast.ToString(getOrReturnDefault("MONGO_DATABASE", "espace"))
+
+	return cfg
+}
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+	if exists {
+		return os.Getenv(key)
+	}
+	return defaultValue
+}
